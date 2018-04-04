ec878ec6141886f630bc10f5c9bea867a9498fdb
Christia Tidwell <christiatidwell@bellsouth.net>
2018-04-04 02:53:14 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,20 +2,20 @@
 
 class Person
 
-def name
-@name
-end
+ def name=(person_name)
+  @name = person_name
+  end
 
-def name=
-@name
-end
+ def name
+  @name
+  end
 
-def job
-@job
-end
+ def job=
+  @job
+  end
 
-def job=
-@job
-end
+ def job
+  @job
+  end
 
 end
